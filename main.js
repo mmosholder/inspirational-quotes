96d866ae6e416175ea6618caa0fd605296b61ea9
Malinda Mosholder <m.m.mosholder@gmail.com>
2015-04-21 14:48:29 -0600

    continued work, quotes are adding to dom

--- main.js
+++ main.js
@@ -1,25 +1,70 @@
 $(document).on('ready', function(){
 
-	var savedQuote = $('<div></div>');
-
 	var Quote = function(text, author) {
 		this.text = text;
 		this.author = author;
+
+		this.render();
 	};
 
+	Quote.prototype.render = function () {
+		if (!this.el) {
+			this.el = $('#quote-tpl')
+			.clone()
+			.attr('id', null)
+			.addClass('quote-block');
+		}
+
+		this.el.find('.quote-text').text(this.text);
+		this.el.find('.author-name').text(this.author);
+
+		return this.el;
+	};
+
+	var QuoteLibrary = function () {
+		this.quotes = [];
+
+		this.render();
+	};
+
+	QuoteLibrary.prototype.addQuote = function(tempQuote) {
+		this.quotes.push(tempQuote);
+
+		this.renderQuoteList();
+
+	};
+
+	QuoteLibrary.prototype.render = function () {
+		if (!this.el) {
+			this.el = $('#library-tpl')
+			.clone()
+			.attr('id', null)
+			.addClass('quote-library');
+		}
+
+		this.renderQuoteList();
+
+		return this.el();
+	};
+
+	QuoteLibrary.prototype.renderQuoteList = function () {
+		var quoteElements = this.quotes.map(function (quote) {
+			return quote.render();
+		});
+
+		this.el.find('.quote-list').append(quoteElements);
+	};
+
+
 	$('.add-quote').on('submit', function (e){
-		Quote.prototype.createNew = function () {
-			var quoteText = $('#quote-text').val();
+		var quoteText = $('#quote-text').val();
+		var authorName = $('#author-text').val();
 
-			var authorText = $('#author-text').val();
+		var tempQuote = new Quote(quoteText, authorName);
 
-			this.newQuote = $('<div')
-				.append(quoteText, authorText);
+		e.preventDefault();
 
-			return this.newQuote;	
-		};
+		$('#quotes-container').append( tempQuote.render() );
 
-		$('.quotes-display').append(this.newQuote);
 	});
-
 });
